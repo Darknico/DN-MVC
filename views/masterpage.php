58dd0766ded50191a2cb15b264684455b96a62c1
Darknico <info@darknico.com>
2018-02-23 05:12:31 +0100

    Added jquery

--- views/masterpage.php
+++ views/masterpage.php
@@ -1,12 +1,24 @@
-<?php if (!defined("DN-MVC")) die("Hacking..."); ?>
+<?php if (!defined("DN-MVC")) {die("Hacking...");}
+?>
 <!DOCTYPE html>
 <html>
 <head>
+	<script type="application/javascript" src="scripts/jQuery/jquery-3.3.1.min.js"></script>
 	<link rel="stylesheet" href="styles/main.css" type="text/css" />
 	<title><?php echo $site_name ?></title>
 </head>
 <body>
 	<img src="img/logo.png" alt="DN-MVC Logo" />
-	<?php $registry->router->loader(); ?>
+	<?php $registry->router->loader();?>
+
+	<div id="jquerytest"></div>
 </body>
 </html>
+
+<script type="application/javascript">
+
+if (typeof jQuery != 'undefined') {
+    $("#jquerytest").html("Use jQuery version "+jQuery.fn.jquery)
+}
+
+</script>
